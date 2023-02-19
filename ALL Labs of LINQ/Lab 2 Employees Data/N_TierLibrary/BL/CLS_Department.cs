a66d98387992bb7851bfb36e8305bb11511f0e13
Adel Hussein Abdelazeem <40367761+adelhussein97@users.noreply.github.com>
2023-02-19 15:57:04 +0200

    Add files via upload

---
+++ ALL Labs of LINQ/Lab 2 Employees Data/N_TierLibrary/BL/CLS_Department.cs
@@ -0,0 +1,54 @@
+﻿using DALContext;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace N_TierLibrary
+{
+   public class CLS_Department
+    {
+        Company_SDEntities db = new Company_SDEntities();
+       
+
+        public System.Collections.IEnumerable Select_Department()
+        {
+            var dept = db.Departments.Select(d=>new {d.Dnum,d.Dname }).ToList();
+            return dept;
+        }
+
+        public void InsertDept(Department dept)
+        {
+            db.Departments.Add(dept);
+            db.SaveChanges();
+           
+           
+
+        }
+        public void UpdateDept(Department dept)
+        {
+            var department = db.Departments.Where(d => d.Dnum == dept.Dnum).FirstOrDefault();
+            department.Dnum = dept.Dnum;
+            department.Dname = dept.Dname;
+            db.SaveChanges();
+
+
+        }
+        public void DeleteDept(int DeptID)
+        {
+            var department = db.Departments.Where(d => d.Dnum == DeptID).FirstOrDefault();
+            db.Departments.Remove(department);
+            db.SaveChanges();
+
+
+        }
+        public int SelectLastDeptID()
+        {
+            var id = db.Departments.Max(d => d.Dnum + 1);
+            return id;
+        }
+
+    }
+}
---
+++ ALL Labs of LINQ/Lab 2 Employees Data/N_TierLibrary/BL/CLS_Employees.cs
@@ -0,0 +1,57 @@
+﻿using DALContext;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace N_TierLibrary
+{
+   public class CLS_Employees
+    {
+        Company_SDEntities db = new Company_SDEntities();
+
+        public System.Collections.IEnumerable Select_Employees()
+        {
+
+            var res= db.Employees.Select(e => new {e.SSN, e.Fname,e.Lname,e.Salary,e.Dno}).ToList();
+            return res;
+            
+        }
+
+        public void InsertEmp(Employee emp)
+        {
+            db.Employees.Add(emp);
+            db.SaveChanges();
+           
+           
+        }
+        public void UpdateEmp(Employee emp)
+        {
+            var employee = db.Employees.Where(e => e.SSN == emp.SSN).FirstOrDefault();
+            employee.SSN = emp.SSN;
+            employee.Fname = emp.Fname;
+            employee.Lname = emp.Lname;
+            employee.Salary = emp.Salary;
+            employee.Dno = emp.Dno;
+            db.SaveChanges();
+
+        }
+        public void DeleteEmp(int EmpID)
+        {
+            
+            var employee = db.Employees.Where(e => e.SSN == EmpID).FirstOrDefault();
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+        }
+
+        public int GetLastID()
+        {
+            var lastid = db.Employees.Max(e => e.SSN + 1);
+            return lastid;
+        }
+
+    }
+}
